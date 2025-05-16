2a7ae7ea812d2407a33fddf9ad70a5e4e37a3a86
Vhanges <115455282+Vhanges@users.noreply.github.com>
2025-05-16 12:27:12 +0800

    Achievement: Learned how to use reusable components. As well as, stack and push method.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Contact Us</title>
+<x-layout>
+    <div class="container">
+        <h2>Contact Us</h2>
+        <form>
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div>
+                <label for="message">Message</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+            </div>
+            <button type="submit">
+                Send Message
+            </button>
+        </form>
+    </div>
+
+    @push('styles')
     <style>
-        body {
+        body {s
             background: #f3f4f6;
             min-height: 100vh;
             display: flex;
@@ -60,27 +78,5 @@
             background: #2563eb;
         }
     </style>
-</head>
-<body>
-    <div class="container">
-        <h2>Contact Us</h2>
-        <form>
-            <div>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" required>
-            </div>
-            <div>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div>
-                <label for="message">Message</label>
-                <textarea id="message" name="message" rows="4" required></textarea>
-            </div>
-            <button type="submit">
-                Send Message
-            </button>
-        </form>
-    </div>
-</body>
-</html>
+    @endpush
+</x-layout>
