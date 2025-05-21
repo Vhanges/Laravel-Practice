84f0433562868da9350d0a0ace420688c4b24e71
Vhanges <115455282+Vhanges@users.noreply.github.com>
2025-05-21 10:54:02 +0800

    Cleared canvas for practicing

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,3 @@ Route::get('/', function(){
     
 })->name('home');
 
-
-Route::get('/jobs', function(){
-
-    return view('jobs', ['jobs' => Jobs::all()] );
-
-})->name('jobs');
-
-
-Route::get('/about', function(){
-
-    return view('about');
-
-})->name('about');
-
-
-Route::get('/jobs/{id}', function($id){
-
-    $job = Jobs::find($id);
-
-    return view('job', ['job' => $job] );
-});
-
-
-Route::get('/contact', function(){
-    return view('contact');
-})->name('contact');
